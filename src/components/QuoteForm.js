e2456cd24fcd254a7c448d674d373fda3a10e19d
Niina Siitari <siitari.niina@gmail.com>
2020-02-18 14:14:34 +0200

    luotu uusi komponentti, QuoteForm, jolla luodaan sitaatteja QuoteList-komponenttiin

---
+++ src/components/QuoteForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class QuoteForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default QuoteForm;
